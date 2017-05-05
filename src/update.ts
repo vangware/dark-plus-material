dd073a81d40125d0e5668dbe7c65148f82270571
Lucas Matías Ciruzzi <hello@lucasmciruzzi.me>
2017-05-05 16:09:56 -0300

    v1.0.0 - New structure and TS update.

---
+++ src/update.ts
@@ -0,0 +1,33 @@
+import { writeFile } from "pn/fs";
+import fetch from "node-fetch";
+import { missingDefaultColors } from "./missingDefaultColors";
+import { defaultsUrl, plusUrl, vsUrl, colorMap } from "./config";
+import { replaceColors, removeDuplicatedColors } from "./helpers";
+import { Theme } from "./interfaces";
+
+Promise
+	.all([
+		defaultsUrl,
+		vsUrl,
+		plusUrl
+	].map(url => fetch(url)))
+	.then(responses => Promise.all<Theme>(responses.map(response => response.json())))
+	.then(([defaults, vs, plus]) => ({
+		defaults: { ...defaults.colors, ...missingDefaultColors },
+		vs: vs.tokenColors,
+		plus: plus.tokenColors
+	}))
+	.then(({ defaults, vs, plus }) => ({
+		colors: Object.keys(defaults).map(key => ({ key, value: defaults[key].toUpperCase() }))
+			.reduce((colors, { key, value }) => ({ ...colors, [key]: colorMap[value] || "MISSING" }), {}),
+		tokenColors: removeDuplicatedColors([...vs, ...plus].map(setting => replaceColors(setting, colorMap)))
+	}))
+	.then(({ colors, tokenColors }) => ({
+		$schema: "vscode://schemas/color-theme",
+		name: "Dark+ Material",
+		colors,
+		tokenColors
+	}))
+	.then(theme => writeFile(`${__dirname}/../dark-plus-material.json`, JSON.stringify(theme)))
+	.then(() => console.log("dark-plus-material.json done!"))
+	.catch(() => console.error("Error with dark-plus-material.json update"))
