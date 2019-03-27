8f1153dfe12c5eed38d4fbf0c5051661a19aac8d
Luke Shiru <hello@lukeshiru.me>
2019-03-27 01:12:20 -0300

    v2.0.6 - Checkout CHANGELOG.md

---
+++ src/config/opacityMap.ts
@@ -0,0 +1,11 @@
+/**
+ * Forced opacity for some visually strong colors.
+ */
+export const opacityMap = {
+	"merge.currentContentBackground": "80",
+	"merge.currentHeaderBackground": "80",
+	"merge.incomingContentBackground": "80",
+	"merge.incomingHeaderBackground": "80"
+};
+
+export default opacityMap;
