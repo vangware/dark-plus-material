a576c2b9eb3c373c98a7b26ac0ff48a75a80dfa6
Lou Cyx <git@lou.cx>
2021-12-27 03:34:38 -0800

    v3.0.1 - Checkout CHANGELOG.md

--- src/isGrayscale.ts
+++
@@ -1,7 +0,0 @@
-import type { RGBA } from "./RGBA.js";
-
-/**
- * Check if given color is grayscale.
- */
-export const isGrayscale = ([red, green, blue]: RGBA) =>
-	red === green && green === blue;
